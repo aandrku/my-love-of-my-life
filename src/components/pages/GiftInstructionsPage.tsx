999d4fccb5572bc8b9c2eee9bcc9267d5260ef11
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-24 16:26:35 -0700

    style gift instructions

--- src/components/pages/GiftInstructionsPage.tsx
+++ src/components/pages/GiftInstructionsPage.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { motion } from 'motion/react';
+import threeHearts from '@/assets/three-hearts.svg';
+import luvU from '@/assets/luv-u.svg';
+import face from '@/assets/face.svg';
+import { useNavigate } from 'react-router';
 
-const place = "under Misha's bed";
+const place = "in your mom's room ";
 
 export function GiftInstructionsPage() {
   const [giftFound, setGiftFound] = useState(false);
   const [giftUnwrapped, setGiftUnwrapped] = useState(false);
+
+  const navigate = useNavigate();
 
   const giftFoundHandler = () => {
     setGiftFound(true);
@@ -16,35 +23,78 @@ export function GiftInstructionsPage() {
 
   if (!giftFound) {
     return (
-      <div>
-        <div>Your gift is {place}</div>
-        <div>Did you find it?</div>
-        <div onClick={giftFoundHandler}>Yes</div>
-      </div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="text-text1 flex h-screen flex-col items-center justify-start gap-8 pt-10 text-2xl"
+      >
+        <div className="bg-bg1 border-accent-border3 relative rounded-xl border p-6 pb-8">
+          <div className="text-3xl">Your gift is {place}</div>
+          <div className="text-text2">Did you find it?</div>
+
+          <motion.div
+            onClick={giftFoundHandler}
+            className="bg-accent-ui1 border-accent-border2 absolute right-3 bottom-3 w-11 -rotate-6 rounded-xl border p-1 text-center text-xl select-none"
+            whileTap={{ rotate: -8, scale: 1.1 }}
+          >
+            Yes
+          </motion.div>
+        </div>
+        <img src={threeHearts} />
+      </motion.div>
     );
   }
 
   if (!giftUnwrapped) {
     return (
-      <div>
-        <div>As you can see, your gift is wrapped.</div>
-        <div>Make sure to unwrap it!</div>
-        <div>Have you unwrapped the gift??</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="text-text1 flex h-screen flex-col items-center justify-start gap-8 pt-10 text-2xl"
+      >
+        <div className="bg-bg1 border-accent-border3 relative rounded-xl border p-6 pb-8">
+          <div className="text-3xl">You kinda have to unwrap it</div>
+          <div className="text-text2">Did you do that?</div>
 
-        <div onClick={giftUnwrappedHandler}>Yes</div>
-      </div>
+          <motion.div
+            onClick={giftUnwrappedHandler}
+            className="bg-accent-ui1 border-accent-border2 absolute right-3 bottom-3 w-11 -rotate-6 rounded-xl border p-1 text-center text-xl select-none"
+            whileTap={{ rotate: -8, scale: 1.1 }}
+          >
+            Yes
+          </motion.div>
+        </div>
+        <img src={luvU} />
+      </motion.div>
     );
   }
 
   return (
-    <div>
-      <div>As you can see your gift is locked ;)</div>
-      <div>
-        I've prepared a few fun challenges for you, so you can have some fun
-        before you get your gift
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="text-text1 flex h-screen flex-col items-center justify-start gap-8 p-2 pt-10 text-2xl"
+    >
+      <div className="bg-bg1 border-accent-border3 relative rounded-xl border p-6 pb-8">
+        <div className="text-3xl">
+          Your gift is locked. You have to complete some challenges to get it
+        </div>
+        <div className="text-text2">Are you ready?</div>
+
+        <motion.div
+          onClick={() => {
+            navigate('/challenge1');
+          }}
+          className="bg-accent-ui1 border-accent-border2 absolute right-3 bottom-3 w-11 -rotate-6 rounded-xl border p-1 text-center text-xl select-none"
+          whileTap={{ rotate: -8, scale: 1.1 }}
+        >
+          Yes
+        </motion.div>
       </div>
-      <div>Are you ready?</div>
-      <div>Yes</div>
-    </div>
+      <img src={face} />
+    </motion.div>
   );
 }
