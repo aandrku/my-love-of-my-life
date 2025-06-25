b6cac665fcb9b6f881f52251c611d2d16ce06fba
Andrii Sozonik <sozonik04.av@gmail.com>
2025-06-24 17:13:43 -0700

    style final message

--- src/components/pages/FinalMessagePage.tsx
+++ src/components/pages/FinalMessagePage.tsx
@@ -1,8 +1,21 @@
+import { motion } from 'motion/react';
+import lotsOfHearts from '@/assets/lots-of-hears.svg';
+import coolHeart from '@/assets/cool-heart.svg';
+
 export function FinalMessagePage() {
   return (
-    <div>
-      <div>I hope you like this gift</div>
-      <div>I love you so much!</div>
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="text-text1 flex flex-col items-center gap-4 p-8 text-center"
+    >
+      <img src={lotsOfHearts} />
+      <div className="text-4xl">
+        You're the <span className="text-accent-text2">best</span> thing that
+        ever happened to me!'
+      </div>
+      <div className="text-accent-text1 text-5xl">Lots of hearts for you</div>
+      <img src={coolHeart} className="size-32" />
+    </motion.div>
   );
 }
